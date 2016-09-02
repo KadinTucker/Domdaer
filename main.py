308a0f22562c30a291dbf778267528716ac3c1cb
Kxoe <Kxoe>
2016-09-02 15:52:51 -0600

    Version 0.2 commit didn't commit main...

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import random
 import pygame
 from pygame.locals import *
 import sys
+try:
+    import objects
+    import world
+except:
+    print('CONSOLE: Missing required modules. Terminating...')
 
-SOUND_NAMES = ['fanfare']
-SOUND_FILES = {'fanfare' : 'Intrepid.mp3'}
+IMAGES = {'uarrowImg' : pygame.image.load('warparrow.png'), 'larrowImg' : pygame.transform.rotate(pygame.image.load('warparrow.png'), 90), 'darrowImg' : pygame.transform.rotate(pygame.image.load('warparrow.png'), 180), 'rarrowImg' : pygame.transform.rotate(pygame.image.load('warparrow.png'), 270)}
 
-MUSIC = {'Intrepid' : 'Intrepid.mp3', 'Summer Day' : 'SummerDay.mp3'}
+MUSIC = {'Intrepid' : 'Intrepid.mp3', 'Summer Day' : 'SummerDay.mp3', 'Janon' : 'Janon.mp3'}
+
+TPS = 200
+CLOCK = pygame.time.Clock()
 
 def terminate():
     """
@@ -14,6 +21,31 @@ def terminate():
     """
     pygame.quit()
     sys.exit()
+
+def checkWarp(room):
+    if PLAYER.coords[0] > 779 and PLAYER.coords[0] < 869:
+        if PLAYER.coords[1] < 30 and room.warps[1] != None:
+            room = world.area(world.world[room.warps[1]])
+            room.buildSurface(resolution)
+            PLAYER.coords = (839, 919)
+            PLAYER.target = (839, 919)
+        elif PLAYER.coords[1] > 949 and room.warps[3] != None:
+            room = world.area(world.world[room.warps[3]])
+            room.buildSurface(resolution)
+            PLAYER.coords = (839, 60)
+            PLAYER.target = (839, 60)
+    elif PLAYER.coords[1] > 459 and PLAYER.coords[1] < 519:
+        if PLAYER.coords[0] < 30 and room.warps[0] != None:
+            room = world.area(world.world[room.warps[0]])
+            room.buildSurface(resolution)
+            PLAYER.coords = (1616, 489)
+            PLAYER.target = (1616, 489)
+        elif PLAYER.coords[0] > 1648 and room.warps[2] != None:
+            room = world.area(world.world[room.warps[2]])
+            room.buildSurface(resolution)
+            PLAYER.coords = (60, 489)
+            PLAYER.target = (60, 489)
+    return room
 
 class button():
     """
@@ -50,7 +82,7 @@ class button():
         """
         display.blit(self.image, self.location)
 
-def checkButtons(buttons, sounds):
+def checkButtons(buttons):
     """
     Checks whether the ID of all buttons being pressed checks out with any pre-determined functions
     of specific buttons. If the function goes through and nothing happens, the button becomes un-pressed.
@@ -69,7 +101,7 @@ def checkButtons(buttons, sounds):
                 print('ERROR: Quit not gone through.')
             if i.identity == 'play':
                 print('CONSOLE: Play button pressed.')
-                setMusic('Summer Day')
+                setMusic('Janon', 0)
                 return 2
         else:
             i.pressed == False
@@ -82,29 +114,52 @@ def initButtons():
     Returns a list of all buttons in the main menu.
     """
     buttons = []
-    buttons.append(button((150, 210), 'load', pygame.image.load('loadgameButton.png')))
-    buttons.append(button((125, 20), None, pygame.image.load('title.png')))
-    buttons.append(button((150, 150), 'play', pygame.image.load('newgameButton.png')))
-    buttons.append(button((150, 270), 'quit', pygame.image.load('quitButton.png')))
+    buttons.append(button((800, 210), 'load', pygame.image.load('loadgameButton.png')))
+    buttons.append(button((775, 20), None, pygame.image.load('title.png')))
+    buttons.append(button((800, 150), 'play', pygame.image.load('newgameButton.png')))
+    buttons.append(button((800, 270), 'quit', pygame.image.load('quitButton.png')))
     return buttons
 
-def initSounds():
-    """
-    Adds a list of sounds to be used in the game.
-    """
-    sounds = {}
-    for sound_name in SOUND_NAMES:
-        sounds[sound_name] = pygame.mixer.Sound(SOUND_FILES[sound_name])
-    return sounds
+##def initSounds():
+##    """
+##    Adds a list of sounds to be used in the game.
+##    """
+##    sounds = {}
+##    for sound_name in SOUND_NAMES:
+##        sounds[sound_name] = pygame.mixer.Sound(SOUND_FILES[sound_name])
+##    return sounds
 
-def setMusic(song):
+def setMusic(song, delay):
     """
     Stops the currently playing song, then sets a new song to be played.
     Takes in the name of the song to be played, a string.
     """
     pygame.mixer.music.stop()
     pygame.mixer.music.load(MUSIC[song])
-    pygame.mixer.music.play(-1)
+    pygame.mixer.music.play(-1, delay)
+
+def displayPlayer(display):
+    display.blit(PLAYER.image, (PLAYER.coords[0] - PLAYER.size, PLAYER.coords[1] - PLAYER.size))
+
+def displayArrows(room, display):
+    if room.warps[1] != None:
+        display.blit(IMAGES['uarrowImg'], (779, 0))
+    if room.warps[3] != None:
+        display.blit(IMAGES['darrowImg'], (779, 948))
+    if room.warps[0] != None:
+        display.blit(IMAGES['larrowImg'], (0, 459))
+    if room.warps[2] != None:
+        display.blit(IMAGES['rarrowImg'], (1648, 459))
+        
+def roomLoop(room, display):
+    for i in room.enemies:
+        display.blit(i.image, (i.coords[0] - i.size, i.coords[1] - i.size))
+        i.AI(PLAYER)
+        i.move()
+        if i.entity_target != None:
+            i.attack(TPS)
+##    for i in room.sceneries:
+##        display.blit(i.image, i.coords)
 
 def initialize():
     """
@@ -113,22 +168,29 @@ def initialize():
     Returns a list of buttons in the main menu, and the starting game state.
     """
     print('CONSOLE: Initializing loop...')
+    global PLAYER, resolution
+    PLAYER = objects.player([3, 5, 20, 10, 50, 15], ['playerGideon.png', 'humandeath.ogg', 'slash.ogg'], (700, 700))
+    resolution = (1678, 978)
+    room = world.area(world.world['janonia4'])
+    room.buildSurface(resolution)
     pygame.init()
     pygame.mixer.init()
-    sounds = initSounds()
-    display = pygame.display.set_mode((500, 400))
+##    sounds = initSounds()
+    display = pygame.display.set_mode(resolution)
+    pygame.display.toggle_fullscreen()
     buttons = initButtons()
     gamestate = 1 #Gamestate 1 is the main menu.
-    setMusic('Intrepid')
+##    setMusic('Intrepid', 3)
     print('CONSOLE: Init complete. Starting loop.')
-    return buttons, gamestate, display, sounds
+    return buttons, gamestate, display, room
 
-def runProgram(buttons, gamestate, display, sounds):
+def runProgram(buttons, gamestate, display, room):
     """
     Run the program main loop
 
     Takes in a list of buttons in the main menu, and the starting game state.
     """
+    setMusic('Intrepid', 0)
     while True:
         if gamestate == 1:
             display.fill((65, 35, 55))
@@ -139,28 +201,39 @@ def runProgram(buttons, gamestate, display, sounds):
                             i.buttonPressed()
                 if event.type == QUIT:
                     terminate()
-            gamestate = checkButtons(buttons, sounds)
+            gamestate = checkButtons(buttons)
             for i in buttons:
                 i.displayButtons(display)
                 
         elif gamestate == 2:
-            display.fill((65, 35, 55))
+##            display.fill((50, 150, 50))
+            display.blit(room.scene, (0, 0))
+            displayPlayer(display)
             for event in pygame.event.get():
                 if event.type == QUIT:
                     terminate()
+                elif event.type == MOUSEBUTTONDOWN:
+                    if event.button == 3:
+                        PLAYER.target_location(room)
+            PLAYER.move()
+            PLAYER.collision(room)
+            roomLoop(room, display)
+            room = checkWarp(room)
+            displayArrows(room, display)
                     
         elif gamestate == 0:
             print('CONSOLE: Quitting game.')
             terminate()
 
+        CLOCK.tick(TPS)
         pygame.display.update()
 
 def main():
     """
     Initialize data, then run the program.
     """
-    buttons, gamestate, display, sounds = initialize()
-    runProgram(buttons, gamestate, display, sounds)
+    buttons, gamestate, display, room = initialize()
+    runProgram(buttons, gamestate, display, room)
 
 if __name__ == '__main__':
     main()
